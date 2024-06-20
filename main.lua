b70490d4b7e46a5b3542c2bc2b9cce3157a88069
unknown <kendrick_sin@hotmail.com>
2024-06-20 20:53:23 +0800

    read from data.csv

--- main.lua
+++ main.lua
@@ -1,8 +1,45 @@
 local http = require('http')
 local fs = require('fs')
 
+-- Function to parse the CSV file
+local function parseCSV(data)
+  local rows = {}
+  for line in data:gmatch("[^\r\n]+") do
+    local row = {}
+    for cell in line:gmatch("[^,]+") do
+      table.insert(row, cell)
+    end
+    table.insert(rows, row)
+  end
+  return rows
+end
+
+-- Function to generate the HTML table
+local function generateTable(data)
+  local tableHTML = [[
+    <table>
+      <tr>
+        <th>Distance</th>
+        <th>Mark1</th>
+        <th>Mark2</th>
+        <th>Mark3</th>
+        <th>Mark4</th>
+      </tr>
+  ]]
+  for _, row in ipairs(data) do
+    tableHTML = tableHTML .. "<tr>"
+    for _, cell in ipairs(row) do
+      tableHTML = tableHTML .. "<td>" .. cell .. "</td>"
+    end
+    tableHTML = tableHTML .. "</tr>"
+  end
+  tableHTML = tableHTML .. "</table>"
+  return tableHTML
+end
+
+-- HTTP request handler
 local function onRequest(req, res)
-  fs.readFile('index.html', function (err, data)
+  fs.readFile('data.csv', function(err, data)
     if err then
       res:writeHead(500, {["Content-Type"] = "text/plain"})
       res:write("500 Internal Server Error\n")
@@ -10,9 +47,23 @@ local function onRequest(req, res)
       return
     end
 
-    res:writeHead(200, {["Content-Type"] = "text/html"})
-    res:write(data)
-    res:finish()
+    local parsedData = parseCSV(data)
+
+    local tableHTML = generateTable(parsedData)
+
+    fs.readFile('index.html', function(err, data)
+      if err then
+        res:writeHead(500, {["Content-Type"] = "text/plain"})
+        res:write("500 Internal Server Error\n")
+        res:finish()
+        return
+      end
+
+      local htmlContent = data:gsub("{{table}}", tableHTML)
+      res:writeHead(200, {["Content-Type"] = "text/html"})
+      res:write(htmlContent)
+      res:finish()
+    end)
   end)
 end
 
